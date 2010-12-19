e0ef2b07f4a988abd753bd31a85d5fe5b9de48e5
Matt Bertolini <viper2843@gmail.com>
2010-12-18 22:24:25 -0500

    Switching tabs to spaces in the camclient package.

--- src/com/mattbertolini/camclient/CamException.java
+++ src/com/mattbertolini/camclient/CamException.java
@@ -1,21 +1,21 @@
 package com.mattbertolini.camclient;
 
 public class CamException extends Exception {
-	private static final long serialVersionUID = 5947975468952957595L;
+    private static final long serialVersionUID = 5947975468952957595L;
 
-	public CamException() {
-		super();
-	}
+    public CamException() {
+        super();
+    }
 
-	public CamException(String message) {
-		super(message);
-	}
-	
-	public CamException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public CamException(String message) {
+        super(message);
+    }
 
-	public CamException(Throwable cause) {
-		super(cause);
-	}
+    public CamException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CamException(Throwable cause) {
+        super(cause);
+    }
 }
