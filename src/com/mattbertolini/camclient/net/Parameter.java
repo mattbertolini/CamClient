05113c9e5f02b7a899f87bcf404153dc60ab6757
Matt Bertolini <viper2843@gmail.com>
2011-03-06 23:25:21 -0500

    Adding javadoc to Parameter interface.

--- src/com/mattbertolini/camclient/net/Parameter.java
+++ src/com/mattbertolini/camclient/net/Parameter.java
@@ -1,5 +1,15 @@
 package com.mattbertolini.camclient.net;
 
+/**
+ * Interface that represents a parameter name that can be sent to the CAM.
+ * 
+ * @author Matt Bertolini
+ */
 public interface Parameter {
+    /**
+     * Gets the text name supported by the CAM.
+     * 
+     * @return The CAM parameter name.
+     */
     String getName();
 }
