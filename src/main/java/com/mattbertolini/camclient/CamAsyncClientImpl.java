842838f72fd47cfb3102907132b20bb40bc161d9
Matt Bertolini <viper2843@gmail.com>
2012-04-21 11:13:53 -0400

    Adding license boilerplate to files. Beginning to add implementation to client.

--- src/main/java/com/mattbertolini/camclient/CamAsyncClientImpl.java
+++ src/main/java/com/mattbertolini/camclient/CamAsyncClientImpl.java
@@ -1,7 +1,36 @@
+/*
+ * Copyright (c) 2012, Matthew Bertolini
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of CamClient nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.mattbertolini.camclient;
 
 import com.mattbertolini.camclient.net.CamConnection;
-import com.mattbertolini.camclient.net.urlconnection.HttpConnectionCamConnection;
 
 import java.net.InetAddress;
 import java.util.concurrent.Callable;
@@ -95,6 +124,39 @@ public class CamAsyncClientImpl extends CamClientImpl implements CamAsyncClient 
             @Override
             public Void call() throws Exception {
                 addSubnet(subnet, mask, type, role, description, ssip);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public Future<Void> bouncePortAsync(final String switchId, final int port) {
+        return this.executorService.submit(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                bouncePort(switchId, port);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public Future<Void> bouncePortByMacAddressAsync(final MacAddress macAddress) {
+        return this.executorService.submit(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                bouncePortByMacAddress(macAddress);
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public Future<Void> changeLoggedInUserRoleAsync(final InetAddress ipAddress, final String role) {
+        return this.executorService.submit(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                changeLoggedInUserRole(ipAddress, role);
                 return null;
             }
         });
--- src/main/java/com/mattbertolini/camclient/CamClientImpl.java
+++ src/main/java/com/mattbertolini/camclient/CamClientImpl.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2012, Matthew Bertolini
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     * Neither the name of CamClient nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.mattbertolini.camclient;
 
 import com.mattbertolini.camclient.net.CamConnection;
@@ -43,7 +73,23 @@ public class CamClientImpl implements CamClient {
 
     @Override
     public void addLocalUser(String username, String password, String role) throws CamClientException {
-        //
+        if(username == null) {
+            throw new IllegalArgumentException("Username cannot be null.");
+        }
+        if(password == null) {
+            throw new IllegalArgumentException("Password cannot be null.");
+        }
+        if(role == null) {
+            throw new IllegalArgumentException("Role cannot be null.");
+        }
+        CamRequest request = new CamRequest(Operation.ADD_LOCAL_USER);
+        request.addParameter(RequestParameter.USERNAME, username);
+        request.addParameter(RequestParameter.USER_PASSWORD, password);
+        request.addParameter(RequestParameter.USER_ROLE, role);
+        CamResponse response = this.connection.submitRequest(request);
+        if(response.isError()) {
+            //
+        }
     }
 
     @Override
@@ -162,6 +208,15 @@ public class CamClientImpl implements CamClient {
     @Override
     public void kickUserByMacAddress(MacAddress macAddress) throws CamClientException {
         //
+        if(macAddress == null) {
+            throw new IllegalArgumentException("MAC address cannot be null.");
+        }
+        CamRequest request = new CamRequest(Operation.KICK_USER_BY_MAC_ADDRESS);
+        request.addParameter(RequestParameter.MAC_ADDRESS, macAddress.toString(MacAddress.Delimiter.NONE));
+        CamResponse response = this.connection.submitRequest(request);
+        if(response.isError()) {
+            //
+        }
     }
 
     @Override
@@ -196,11 +251,6 @@ public class CamClientImpl implements CamClient {
 
     @Override
     public void renewUserSessionTime(InetAddress ipAddress) throws CamClientException {
-        //
-    }
-
-    @Override
-    public void setUserAgent(String userAgent) {
         //
     }
 
